1cbd0449d5cccbb0394c298ae53742eb16a71b93
Hlamong55 <bijoymarma55@gmail.com>
2025-09-26 22:45:38 +0600

    banner & cards section added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import { useState } from "react";
 import Footer from "./components/Footer"
 import Navbar from "./components/navbar"
+import Progress from "./components/Progress"
+import Tickets from "./components/Ticket";
 
 function App() {
+
+  const [tickets, setTickets] = useState([])
+fetch("/Ticket.json")
+.then((res) => (res.json()))
+.then((data) => setTickets(data));
+console.log(tickets);
 
   return (
     <div>
      <Navbar />
+
+    <div className="min-h-screen">
+    <Progress />
+    <Tickets tickets={tickets} />
+    </div>
 
      <Footer />
     </div>
